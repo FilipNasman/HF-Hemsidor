4a8778835d90f388cb1bc8bdcc7c5c0a1d972b37
FilipNasman <filip.nasman@gmail.com>
2022-02-08 22:56:45 +0100

    hhuggge update  wwith burger mernue

--- index.php
+++ index.php
@@ -4,15 +4,23 @@
     <meta charset="UTF-8">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
 </head>
 <body>
-    <div class="main"> 
-      <div class="header">
-         <img class="logo" url="logo.png"></img>
-          </div>
-        <div class="image"></div>
-          
-      </div>  
-    </div>
+    <nav class="navbar">
+      <div class="logo">HF Hemsidor</div>
+      <a href="#" class="toggle-button">
+        <span class="bar"></span>
+        <span class="bar"></span>
+        <span class="bar"></span> 
+      </a>
+      <div class="navbar-links">
+        <ul>
+          <li><a href="#">Hem</a></li>
+          <li><a href="#">Om oss</a></li>
+          <li><a href="#">Kontakta oss</a></li>
+        </ul>
+      </div>
+    </nav>  
 </body>
 </html>
